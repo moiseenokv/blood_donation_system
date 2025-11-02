4246fc3858bdc9dac0a109a2b55ced3509793d5d
Vitali Maisiayonak <moiseenokv@gmail.com>
2025-11-02 11:32:48 +0300

    init: install last next.js(app-router, tailwind, typescript) version with a little cleanup

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+  display: 'swap',
+  weight: ['400', '500', '600', '700'],
+})
+
+export const metadata: Metadata = {
+  title: "Blood Donation Appp",
+  description: "Simple donation App with Next.js ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${montserat.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {}
+
+function HomePage(props: Props) {
+  const {} = props
+
+  return (
+    <div className='p-5'>Homepage</div>
+  )
+}
+
+export default HomePage
